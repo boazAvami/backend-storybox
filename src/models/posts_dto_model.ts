0b32d8aa4862136a3897b03268048e0a9a0d812a
ShirinYa <shirinyashar@gmail.com>
2025-03-11 19:18:37 +0200

    fix names

--- src/models/posts_dto_model.ts
+++ src/models/posts_dto_model.ts
@@ -2,10 +2,10 @@ class PostDto {
     _id: string;
     content: string;
     ownerId: string;
-    imageUri: string;
-    createdAt: Date;
-    likeCount: number;
-    commentCount: number;
+    image_uri: string;
+    created_at: Date;
+    like_count: number;
+    comment_count: number;
     tags: string[];
     isLikedByMe: boolean; 
 
@@ -13,10 +13,10 @@ class PostDto {
         this._id = post.id.toString(); // Converting ObjectId to string if needed
         this.content = post.content;
         this.ownerId = post.ownerId.toString(); // Convert ObjectId to string
-        this.imageUri = post.image_uri || "";
-        this.createdAt = post.created_at || new Date();
-        this.likeCount = post.like_count || 0;
-        this.commentCount = post.comment_count || 0;
+        this.image_uri = post.image_uri || "";
+        this.created_at = post.created_at || new Date();
+        this.like_count = post.like_count || 0;
+        this.comment_count = post.comment_count || 0;
         this.tags = post.tags || [];
         this.isLikedByMe = isLikedByMe;
     }
