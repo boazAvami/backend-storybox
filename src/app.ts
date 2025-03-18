7d80ea37e65b1c6b90af2c6bfc9579c237f9a130
boazAvami <boaz.avami@gmail.com>
2025-03-18 23:32:53 +0200

    https socket

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,20 @@
 import { initApp } from "./server";
-import https from 'https';
-import http from 'http';
-import fs from 'fs';
+import {initHttpsApp} from "./httpsServer";
 
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 4000;
+if (process.env.NODE_ENV !== "production") {
+    initApp().then(({ app, server }) => {
+            console.log("Development Mode");
+            server.listen(port, () => {
+                console.log(`Server listening at http://localhost:${port}`);
+            });
+        });
+} else {
+    initHttpsApp().then(({ app, httpsServer }) => {
+            console.log("PRODUCTION Mode");
+            httpsServer.listen(port, '0.0.0.0', () => {
+                console.log(`Server listening at https://0.0.0.0:${port}`);
+            });
+        });
+}
 
-initApp().then(({ app, server }) => {
-    if (process.env.NODE_ENV !== "production") {
-        console.log("Development Mode");
-        server.listen(port, () => {
-            console.log(`Server listening at http://localhost:${port}`);
-        });
-    } else {
-        console.log("PRODUCTION Mode");
-        const options = {
-            key: fs.readFileSync('./client-key.pem'),
-            cert: fs.readFileSync('./client-cert.pem'),
-        };
-        https.createServer(options, app).listen(port, '0.0.0.0', () => {
-            console.log(`Server listening at https://0.0.0.0:${port}`);
-        });
-    }
-});
